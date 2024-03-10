a84d3f0cad517bacdfc5aef4415ae6ab0ba1424d
Mohammadyusuf <mohammedyousef.dev@gmail.com>
2024-03-10 18:01:19 +0500

    Confirm with admin

--- users/signals.py
+++ users/signals.py
@@ -2,8 +2,12 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Application, Day
 from datetime import datetime
+from .tasks import send_message_to_admin
 
-from .utils import send_message
+from .utils import send_message, send_photo
+
+BASE_URL = "http://127.0.0.1:8000/"
+ADMIN = 1921103181
 
 
 @receiver(post_save, sender=Application)
@@ -22,7 +26,8 @@ def check_field_on_save(sender, instance, created, **kwargs):
 
         elif not instance.is_approved:
             try:
-                days = set(x.strip() for x in instance.days.replace("}", "").replace("{", "").replace("'", "").split(","))
+                days = set(
+                    x.strip() for x in instance.days.replace("}", "").replace("{", "").replace("'", "").split(","))
             except:
                 days = set(instance.days)
 
@@ -33,3 +38,63 @@ def check_field_on_save(sender, instance, created, **kwargs):
                     day_data.users.remove(instance.user)
                 except Exception as e:
                     print(e)
+
+    if instance.payment_check_url and instance._sent_to_admin == False:
+        url = BASE_URL + f"users/application/{instance.id}/change/"
+        caption = f"New application from {instance.user.first_name} {instance.user.last_name}\n\n"
+        caption += f"Phone: {instance.phone}"
+        if instance.phone2:
+            caption += f", {instance.phone2}\n"
+        else:
+            caption += "\n"
+
+        caption += f"Payment type: {instance.get_payment_type_display()}"
+
+        try:
+            days = set(
+                x.strip() for x in instance.days.replace("}", "").replace("{", "").replace("'", "").split(","))
+        except:
+            days = set(instance.days)
+
+        if days:
+            caption += "\nDays:\n"
+            caption += '\n'.join(days)
+
+        caption += "\n\n====================="
+        caption += "\n Confirm application"
+        caption += f"\n{url}"
+
+        with_photo = True
+
+        send_message_to_admin.delay(ADMIN, instance.payment_check_url, caption, instance.id, with_photo)
+
+    if not instance.payment_check_url and instance._sent_to_admin == False and created and instance.payment_type == "cash":
+        url = BASE_URL + f"users/application/{instance.id}/change/"
+        caption = f"New application from {instance.user.first_name} {instance.user.last_name}\n\n"
+        caption += f"Phone: {instance.phone}"
+        if instance.phone2:
+            caption += f", {instance.phone2}\n"
+        else:
+            caption += "\n"
+
+        caption += f"Payment type: {instance.get_payment_type_display()}"
+
+        try:
+            days = set(
+                x.strip() for x in instance.days.replace("}", "").replace("{", "").replace("'", "").split(","))
+        except:
+            days = set(instance.days)
+
+        print(days)
+
+        if days:
+            caption += "\nDays:\n"
+            caption += '\n'.join(days)
+
+        caption += "\n\n====================="
+        caption += "\n Confirm application"
+        caption += f"\n{url}"
+
+        with_photo = False
+
+        send_message_to_admin.delay(ADMIN, instance.payment_check_url, caption, instance.id, with_photo)
--- users/utils.py
+++ users/utils.py
@@ -1,8 +1,36 @@
-from telegram import Bot
+import requests
+from telegram import Bot, InlineKeyboardButton, InlineKeyboardMarkup
 
 from dtb.settings import TELEGRAM_TOKEN
 
 TELEGRAM_TOKEN = "6432845948:AAHqwCgoO-J7fWkOO3E7fDHoqCdeE5I6wXI"
+
+
 def send_message(chat_id, message):
     bot = Bot(TELEGRAM_TOKEN)
     bot.send_message(chat_id, message)
+
+
+def send_photo(chat_id, photo, message, order_id):
+    bot = Bot(TELEGRAM_TOKEN)
+    photo_response = requests.get(photo)
+    keyboard = [
+        [InlineKeyboardButton("✅ Confirm", callback_data=f'confirm-{order_id}')],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    # Check if the request was successful
+    if photo_response.status_code == 200:
+        bot.send_photo(chat_id=chat_id, photo=photo_response.content, caption=message, reply_markup=reply_markup)
+
+
+def send_confirm_text(chat_id, message, order_id):
+    print("Text worked")
+    bot = Bot(TELEGRAM_TOKEN)
+
+    keyboard = [
+        [InlineKeyboardButton("✅ Confirm", callback_data=f'confirm-{order_id}')],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    bot.send_message(chat_id=chat_id, text=message, reply_markup=reply_markup)
